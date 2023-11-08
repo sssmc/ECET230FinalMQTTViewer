ee638d643b203914a7b90a9ef8a643a9d1cbdf6e
Sebastien Robitaille <sebastien@robitaille.info>
2023-11-07 22:20:01 -0800

    Added basic props and methods for MQTTConnection

--- MQTTServerConnection/Connection.cs
+++ MQTTServerConnection/Connection.cs
@@ -2,6 +2,35 @@
 {
     public class Connection
     {
+        public string Name { get; set; }
 
+        public string ClientId { get; set; }
+
+        public string Host { get; set; }
+
+        public int Port { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public Connection(string name, string clientId, string host, int port, string username, string password)
+        {
+            Name = name;
+            ClientId = clientId;
+            Host = host;
+            Port = port;
+            Username = username;
+            Password = password;
+        }
+
+        public string getThinkspeakTopicString(string channelNumber, string field)
+        {
+            return $"channels/{channelNumber}/subscribe/fields/{field}";
+        }
+        public string[] getValidThinkspeakTopicFields()
+        {
+            return new string[] { "field1", "field2", "field3", "field4", "field5", "field6", "field7", "field8" };
+        }
     }
 }
