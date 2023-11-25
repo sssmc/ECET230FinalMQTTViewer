0d9759ba9e25c006c7fef1e3d90e8042e6fef3d5
Sebastien Robitaille <sebastien@robitaille.info>
2023-11-24 21:32:45 -0800

    screen edit view and view model cleanup

--- ECET230FinalMQTTViewerDesktop/ViewModels/ScreenEditViewModel.cs
+++ ECET230FinalMQTTViewerDesktop/ViewModels/ScreenEditViewModel.cs
@@ -2,38 +2,30 @@
 using CommunityToolkit.Mvvm.Input;
 using ECET230FinalMQTTViewerDesktop.Models;
 using MQTTScreenData;
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Text.Json;
-using System.Threading.Tasks;
-using static ECET230FinalMQTTViewerDesktop.ViewModels.ScreenEditViewModel;
 
 namespace ECET230FinalMQTTViewerDesktop.ViewModels
 {
     partial class ScreenEditViewModel : ObservableValidator
     {
 
-        public ScreenDataModel _screenDataModel { get; set; }
-
+        [ObservableProperty]
+        private ScreenDataModel _screenDataModel;
 
         [ObservableProperty]
-        [Required]
-        [Range(0, int.MaxValue)]
         public int _currentScreenIndex;
 
         [ObservableProperty]
         private int _currentIndicatorIndex;
 
+        [ObservableProperty]
+        private IndicatorData _selectedIndicator;
+
         public string[] ScreenNames
         {
             get
             {
-                string[] screenNames = new string[_screenDataModel.ScreenCount];
-                for (int i = 0; i < _screenDataModel.ScreenCount; i++)
+                string[] screenNames = new string[ScreenDataModel.ScreenCount];
+                for (int i = 0; i < ScreenDataModel.ScreenCount; i++)
                 {
                     screenNames[i] = $"Screen {i + 1}";
                 }
@@ -45,160 +37,12 @@ namespace ECET230FinalMQTTViewerDesktop.ViewModels
         {
             get
             {
-                string[] indicatorNames = new string[_screenDataModel.GetIndicatorsAtScreenIndex(CurrentScreenIndex).Length];
-                for (int i = 0; i < _screenDataModel.GetIndicatorsAtScreenIndex(CurrentScreenIndex).Length; i++)
+                string[] indicatorNames = new string[ScreenDataModel.GetIndicatorsAtScreenIndex(CurrentScreenIndex).Length];
+                for (int i = 0; i < ScreenDataModel.GetIndicatorsAtScreenIndex(CurrentScreenIndex).Length; i++)
                 {
-                    indicatorNames[i] = _screenDataModel.GetIndicatorsAtScreenIndex(CurrentScreenIndex)[i].Name;
+                    indicatorNames[i] = ScreenDataModel.GetIndicatorsAtScreenIndex(CurrentScreenIndex)[i].Name;
                 }
                 return indicatorNames;
-            }
-        }
-
-        public string CurrentIndicatorName
-        {
-            get
-            {
-                return _screenDataModel.GetIndicatorsAtScreenIndex(Math.Clamp(CurrentScreenIndex, 0, int.MaxValue))[Math.Clamp(CurrentIndicatorIndex, 0, int.MaxValue)].Name;
-            }
-            set
-            {
-
-                _screenDataModel.GetIndicatorsAtScreenIndex(Math.Clamp(CurrentScreenIndex, 0, int.MaxValue))[Math.Clamp(CurrentIndicatorIndex, 0, int.MaxValue)].Name = value;
-
-            }
-        }
-
-        public string CurrentIndicatorTopic
-        {
-            get
-            {
-                return _screenDataModel.GetIndicatorsAtScreenIndex(Math.Clamp(CurrentScreenIndex, 0, int.MaxValue))[Math.Clamp(CurrentIndicatorIndex, 0, int.MaxValue)].Topic;
-            }
-            set
-            {
-
-                _screenDataModel.GetIndicatorsAtScreenIndex(Math.Clamp(CurrentScreenIndex, 0, int.MaxValue))[Math.Clamp(CurrentIndicatorIndex, 0, int.MaxValue)].Topic = value;
-            }
-        }
-
-        public string CurrentIndicatorType
-        {
-            get
-            {
-                return _screenDataModel.GetIndicatorsAtScreenIndex(Math.Clamp(CurrentScreenIndex, 0, int.MaxValue))[Math.Clamp(CurrentIndicatorIndex, 0, int.MaxValue)].Type;
-            }
-            set
-            {
-
-                _screenDataModel.GetIndicatorsAtScreenIndex(Math.Clamp(CurrentScreenIndex, 0, int.MaxValue))[Math.Clamp(CurrentIndicatorIndex, 0, int.MaxValue)].Type = value;
-            }
-        }
-
-        public int CurrentIndicatorMax
-        {
-            get
-            {
-                return _screenDataModel.GetIndicatorsAtScreenIndex(Math.Clamp(CurrentScreenIndex, 0, int.MaxValue))[Math.Clamp(CurrentIndicatorIndex, 0, int.MaxValue)].MaxValue;
-            }
-            set
-            {
-                _screenDataModel.GetIndicatorsAtScreenIndex(Math.Clamp(CurrentScreenIndex, 0, int.MaxValue))[Math.Clamp(CurrentIndicatorIndex, 0, int.MaxValue)].MaxValue = value;
-            }
-        }
-
-        public int CurrentIndicatorMin
-        {
-            get
-            {
-                return _screenDataModel.GetIndicatorsAtScreenIndex(Math.Clamp(CurrentScreenIndex, 0, int.MaxValue))[Math.Clamp(CurrentIndicatorIndex, 0, int.MaxValue)].MinValue;
-            }
-            set
-            {
-                _screenDataModel.GetIndicatorsAtScreenIndex(Math.Clamp(CurrentScreenIndex, 0, int.MaxValue))[Math.Clamp(CurrentIndicatorIndex, 0, int.MaxValue)].MinValue = value;
-            }
-        }
-
-        public string WiFiSSID
-        {
-            get
-            {
-                return _screenDataModel.Connection.WifiSSID;
-            }
-            set
-            {
-                _screenDataModel.Connection.WifiSSID = value;
-            }
-        }
-
-        public string WiFiPassword
-        {
-            get
-            {
-                return _screenDataModel.Connection.WifiPassword;
-            }
-            set
-            {
-                _screenDataModel.Connection.WifiPassword = value;
-            }
-        }
-
-        public string MQTTBrokerAddress
-        {
-            get
-            {
-                return _screenDataModel.Connection.MQTTHost;
-            }
-            set
-            {
-                _screenDataModel.Connection.MQTTHost = value;
-            }
-        }
-
-        public int MQTTBrokerPort
-        {
-            get
-            {
-                return _screenDataModel.Connection.MQTTPort;
-            }
-            set
-            {
-                _screenDataModel.Connection.MQTTPort = value;
-            }
-        }
-
-        public string MQTTBrokerUsername
-        {
-            get
-            {
-                return _screenDataModel.Connection.MQTTUsername;
-            }
-            set
-            {
-                _screenDataModel.Connection.MQTTUsername = value;
-            }
-        }
-
-        public string MQTTBrokerPassword
-        {
-            get
-            {
-                return _screenDataModel.Connection.MQTTPassword;
-            }
-            set
-            {
-                _screenDataModel.Connection.MQTTPassword = value;
-            }
-        }
-
-        public string MQTTClientID
-        {
-            get
-            {
-                return _screenDataModel.Connection.MQTTClientId;
-            }
-            set
-            {
-                _screenDataModel.Connection.MQTTClientId = value;
             }
         }
 
@@ -206,77 +50,49 @@ namespace ECET230FinalMQTTViewerDesktop.ViewModels
         {
             get
             {
-                return _screenDataModel.GetIndicatorsAtScreenIndex(CurrentScreenIndex).ToList();
+                return ScreenDataModel.GetIndicatorsAtScreenIndex(CurrentScreenIndex).ToList();
             }
         }
 
-        [ObservableProperty]
-        private IndicatorData _selectedIndicator;
-
         public ScreenEditViewModel()
         {
-            _screenDataModel = App.screenDataModel;
-            _currentScreenIndex = 0;
-            _screenDataModel.ScreenDataUpdated += _screenDataModel_ScreenDataUpdated;
-            _screenDataModel.RequestScreenDataFromDevice();
-
-            //IndicatorGroups = new List<IndicatorGroup>();
-            //IndicatorGroups.Add(new IndicatorGroup("Screen 1", new ObservableCollection<IndicatorData>(_screenDataModel.GetIndicatorsAtScreenIndex(0).ToList())));
-            //IndicatorGroups.Add(new IndicatorGroup("Screen 1", new ObservableCollection<IndicatorData>(_screenDataModel.GetIndicatorsAtScreenIndex(1).ToList())));
-
+            ScreenDataModel = App.screenDataModel;
+            CurrentScreenIndex = 0;
+            ScreenDataModel.ScreenDataUpdated += _screenDataModel_ScreenDataUpdated;
+            ScreenDataModel.RequestScreenDataFromDevice();
         }
 
         private void _screenDataModel_ScreenDataUpdated(object sender, EventArgs e)
-        {
-            
-
-
-            UpdateIndicatorGroups();
-        }
-
-        private void UpdateIndicatorGroups()
-        {
-
-            OnPropertyChanged(nameof(CurrentIndicatorName));
-            OnPropertyChanged(nameof(CurrentIndicatorTopic));
-            OnPropertyChanged(nameof(CurrentIndicatorType));
-            OnPropertyChanged(nameof(CurrentIndicatorMax));
-            OnPropertyChanged(nameof(CurrentIndicatorMin));
-            OnPropertyChanged(nameof(CurrentScreenIndicatorNames));
+        {    
             OnPropertyChanged(nameof(ScreenNames));
-            OnPropertyChanged(nameof(WiFiSSID));
-            OnPropertyChanged(nameof(WiFiPassword));
-            OnPropertyChanged(nameof(MQTTBrokerAddress));
-            OnPropertyChanged(nameof(MQTTBrokerPort));
-            OnPropertyChanged(nameof(MQTTBrokerUsername));
-            OnPropertyChanged(nameof(MQTTBrokerPassword));
-            OnPropertyChanged(nameof(MQTTClientID));
+            OnPropertyChanged(nameof(CurrentScreenIndicators));
+            CurrentScreenIndex = 0; 
+            SelectedIndicator = null;
         }
 
         partial void OnCurrentScreenIndexChanged(int oldValue, int newValue)
         {
             OnPropertyChanged(nameof(CurrentScreenIndicatorNames));
             OnPropertyChanged(nameof(CurrentScreenIndicators));
-            CurrentIndicatorIndex = 0;
 
         }
 
         [RelayCommand]
         void ProgramScreenData()
         {
-            _screenDataModel.SendScreenDataToDevice();
+            ScreenDataModel.SendScreenDataToDevice();
         }
 
         [RelayCommand]
         void RequestScreenData()
         {
-            _screenDataModel.RequestScreenDataFromDevice();
+            ScreenDataModel.RequestScreenDataFromDevice();
         }
 
         [RelayCommand]
         void AddIndicator()
         {
-            _screenDataModel.SetIndicatorsAtScreenIndex(CurrentScreenIndex, _screenDataModel
+            ScreenDataModel.SetIndicatorsAtScreenIndex(CurrentScreenIndex, ScreenDataModel
                             .GetIndicatorsAtScreenIndex(CurrentScreenIndex)
                             .Append(new IndicatorData("New Indicator", "New Indicator", "New Indicator", "numeric", 100, 0))
                             .ToArray());
@@ -287,12 +103,12 @@ namespace ECET230FinalMQTTViewerDesktop.ViewModels
         [RelayCommand]
         void RemoveIndicator()
         {
-            List<IndicatorData> indicators = _screenDataModel.GetIndicatorsAtScreenIndex(CurrentScreenIndex)
+            List<IndicatorData> indicators = ScreenDataModel.GetIndicatorsAtScreenIndex(CurrentScreenIndex)
                                                              .ToList<IndicatorData>();
 
             indicators.Remove(SelectedIndicator);
 
-            _screenDataModel.SetIndicatorsAtScreenIndex(CurrentScreenIndex, indicators.ToArray());
+            ScreenDataModel.SetIndicatorsAtScreenIndex(CurrentScreenIndex, indicators.ToArray());
 
             OnPropertyChanged(nameof(CurrentScreenIndicators));
         }
@@ -300,17 +116,15 @@ namespace ECET230FinalMQTTViewerDesktop.ViewModels
         [RelayCommand]
         void AddScreen()
         {
-            _screenDataModel.AddScreen();
-
-            UpdateIndicatorGroups();
+            ScreenDataModel.AddScreen();
+            OnPropertyChanged(nameof(ScreenNames));
         }
 
         [RelayCommand]
         void RemoveScreen()
         {
-            _screenDataModel.RemoveScreen(CurrentScreenIndex);
+            ScreenDataModel.RemoveScreen(CurrentScreenIndex);
             OnPropertyChanged(nameof(ScreenNames));
-            UpdateIndicatorGroups();
         }
     }
 }
